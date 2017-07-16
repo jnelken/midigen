d319f5c35bb919102c40ccd4d5c8ec9f5c431dd0
Jake Nelken <jnelks@gmail.com>
2017-07-16 16:37:32 -0400

    Add Midi connection.

--- src/App.js
+++ src/App.js
@@ -3,6 +3,29 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    // https://www.keithmcmillen.com/blog/making-music-in-the-browser-web-midi-api/
+    if (navigator.requestMIDIAccess) {
+      navigator.requestMIDIAccess({
+        sysex: false // this defaults to 'false' and we won't be covering sysex in this article.
+      }).then(this.onMIDISuccess.bind(this), this.onMIDIFailure.bind(this))
+    } else {
+      alert("No MIDI support in your browser.");
+    }
+  }
+
+  onMIDISuccess(midiAccess) {
+    // when we get a succesful response, run this code
+    console.log('MIDI Access Object', midiAccess);
+    this.setState({ midi: midiAccess })
+  }
+
+  onMIDIFailure(e) {
+    // when we get a failed response, run this code
+    console.log("No access to MIDI devices or your browser doesn't support WebMIDI API. Please use WebMIDIAPIShim " + e);
+  }
+
   render() {
     return (
       <div className="App">
